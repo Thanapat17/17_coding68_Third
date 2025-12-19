d53a02991599ca1178181f8c2c83c3a3e11fa5f8
Thanapat17 <26409@bbw.ac.th>
2025-12-19 16:00:09 +0700

    สร้างโดยใช้ Colab

---
+++ mid-17_Third.py
@@ -0,0 +1,56 @@
+{
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Thanapat17/17_coding68_Third/blob/main/mid-17_Third.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "eyesight = float(input(\"กรอกค่าสายตา \"))\n",
+        "\n",
+        "if eyesight < 1.0:\n",
+        "  print(\"ควรไปตรวจวัดสายตา\")\n",
+        "else:\n",
+        "  print(\"ไม่ต้องไปตรวจวัดสายตา\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "i1IzrsBls5mu",
+        "outputId": "a2d58891-9f4b-499d-a4d8-b415f994fa56"
+      },
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "กรอกค่าสายตา 0.3\n",
+            "ควรไปตรวจวัดสายตา\n"
+          ]
+        }
+      ]
+    }
+  ],
+  "metadata": {
+    "colab": {
+      "name": "ยินดีต้อนรับสู่ Colab",
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
